5a3118b5803eeef784c4046986957013e02d524b
David Cabal <dncabal@gmail.com>
2019-08-29 19:23:58 -0500

    "/gif" command support in chat poc

--- src/chat/chat.ts
+++ src/chat/chat.ts
@@ -1,13 +1,23 @@
 const sendMessage = (socket): void => {
   const message = (<HTMLInputElement>document.getElementById('m')).value;
-  socket.emit('message', `${myName}: ${message}`);
+  if (!isCommand(message)) {
+    socket.emit('message', `${myName}: ${message}`);
+    appendMyMessage(message);
+  } else {
+    socket.emit('sendGif', '');
+  }
   (<HTMLInputElement>document.getElementById('m')).value = '';
-  appendMyMessage(message);
 };
 
 const receiveMessages = (socket): void => {
   socket.on('message', msg => {
     appendOtherMessage(msg);
+  });
+
+  socket.on('gif', url => {
+    document.getElementById('gif').style.display = 'inline-block';
+    (<HTMLImageElement>document.getElementById('gif')).src = `https://giphy.com/embed/${url}`;
+    setTimeout(() => document.getElementById('gif').style.display = 'none', 5000);
   });
 };
 
@@ -17,6 +27,7 @@ const appendMyMessage = (msg: string): void => {
   li.setAttribute('class', 'mine');
   li.appendChild(msgElement);
   messageList.appendChild(li);
+  messageList.scrollTop = messageList.scrollHeight;
 };
 
 const appendOtherMessage = (msg: string): void => {
@@ -25,15 +36,18 @@ const appendOtherMessage = (msg: string): void => {
   li.setAttribute('class', 'theirs');
   li.appendChild(msgElement);
   messageList.appendChild(li);
+  messageList.scrollTop = messageList.scrollHeight;
 };
+
+const isCommand = (msg: string): boolean => msg === '/gif';
 
 const messageList = document.getElementById('messages');
 
-const randomUserName = 'user' + Math.floor(Math.random() * (10000 - 1000) + 1000);
+const randomUserName: string = 'user' + Math.floor(Math.random() * (10000 - 1000) + 1000);
 
-const myName = window.prompt('Enter your name', randomUserName) || randomUserName;
+const myName: string = window.prompt('Enter your name', randomUserName) || randomUserName;
 
-const detectKeyPress = (key, socket) => {
+const detectKeyPress = (key, socket): void => {
   if (key.keyCode && key.keyCode === 13) {
     sendMessage(socket);
   }
--- src/chat/setup.ts
+++ src/chat/setup.ts
@@ -1,8 +1,10 @@
+const nodeFetch = require('node-fetch');
+
 export const initializeChat = (io): void => {
   io.on('connection', function (socket) {
-    // do something fun when a user connects
+    io.emit('message', 'A new user has connected!');
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
       // do something really sad when a user disconnects
     });
 
@@ -10,5 +12,16 @@ export const initializeChat = (io): void => {
       // when a message comes in on the "message" channel, emit it out to everyone that is connected
       socket.broadcast.emit('message', msg);
     });
+
+    socket.on('sendGif', async () => {
+      const gifUrl = await getRandomGif();
+      io.emit('gif', gifUrl);
+    });
   });
+};
+
+const getRandomGif = async () => {
+  const response = await nodeFetch('https://api.giphy.com/v1/gifs/random?api_key=iy4oHQZmxRq8KH1x6S2yeeWIyh54ahGY&tag=fail&rating=G');
+  const gifObject = await response.json();
+  return gifObject.data.id;
 };
